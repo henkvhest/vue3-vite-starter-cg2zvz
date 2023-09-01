6fe1145270c22d0ee256f0a914a7737b4357934d
henkvhest <henkvhest@hotmail.com>
2023-09-01 14:16:34 +0200

    Initial commit

---
+++ src/components/List.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul class="list">
+    <slot></slot>
+    <li>a</li>
+    <li>b</li>
+    <li>c</li>
+  </ul>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<style lang="scss" scoped>
+.list > li + li {
+  margin-top: 16px;
+  padding: 0;
+}
+</style>
